3a9bcc06302f2fcb13da18fd88b0bd039439bbbd
Danny Avery <danny.w.avery@gmail.com>
2020-07-16 09:29:17 -0500

    initial commit

---
+++ testingsanctumclient/src/main.js
@@ -0,0 +1,17 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = "http://localhost:8000/";
+
+Vue.config.productionTip = false;
+store.dispatch("auth/me").then(() => {
+  new Vue({
+    router,
+    store,
+    render: (h) => h(App),
+  }).$mount("#app");
+});
